63c3a67c340a34cfac9e433eee207c8805898346
Nils Quirmbach <nquirmbach@googlemail.com>
2024-07-31 22:13:24 +0200

    Initial commit

---
+++ MyAspireApp/MyAspireApp.AppHost/Program.cs
@@ -0,0 +1,9 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var apiService = builder.AddProject<Projects.MyAspireApp_ApiService>("apiservice");
+
+builder.AddProject<Projects.MyAspireApp_Web>("webfrontend")
+    .WithExternalHttpEndpoints()
+    .WithReference(apiService);
+
+builder.Build().Run();
